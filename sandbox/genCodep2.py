7c104d94e37bc82a9458e4ba0a82a14c0800df58
ftarlaci <ftarlaci@utexas.edu>
2017-06-12 22:07:33 -0700

    ppt and binary trees

---
+++ sandbox/genCodep2.py
@@ -0,0 +1,13 @@
+import random
+import json
+from tree import *
+import numpy as np
+import operator
+from htmlGenerator import *
+
+NPROGRAMS = 1000000
+
+
+# Generate code for the decision tree of p2
+
+def main():
